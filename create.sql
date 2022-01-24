f100439e61f479500352f4787a3f761abf5b4232
Martin Gold <goldma1@uhk.cz>
2022-01-24 20:23:53 +0100

    feat: boilerplate templates

--- create.sql
+++ create.sql
@@ -13,3 +13,12 @@ create table user (id bigint not null, created_at datetime(6), email varchar(255
 create table hibernate_sequence (next_val bigint) engine=InnoDB;
 insert into hibernate_sequence values ( 1 );
 create table user (id bigint not null, created_at datetime(6), email varchar(255), password varchar(255), username varchar(255), primary key (id)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table user (id bigint not null, created_at datetime(6), email varchar(255), password varchar(255), username varchar(255), primary key (id)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table user (id bigint not null, created_at datetime(6), email varchar(255), password varchar(255), username varchar(255), primary key (id)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table user (id bigint not null, created_at datetime(6), email varchar(255), password varchar(255), username varchar(255), primary key (id)) engine=InnoDB;
--- src/main/kotlin/cz/uhk/ppro/betoffice/controller/IndexController.kt
+++ src/main/kotlin/cz/uhk/ppro/betoffice/controller/IndexController.kt
@@ -15,4 +15,24 @@ class IndexController(private val userRepository: UserRepository) {
 		return "index"
 	}
 
+	@GetMapping("/matches")
+	fun matchesList(model: Model): String {
+		return "matchesList"
+	}
+
+	@GetMapping("/login")
+	fun login(model: Model): String {
+		return "login"
+	}
+
+	@GetMapping("/bet")
+	fun bet(model: Model): String {
+		return "bet"
+	}
+
+	@GetMapping("/profile")
+	fun profile(model: Model): String {
+		return "profile"
+	}
+
 }
