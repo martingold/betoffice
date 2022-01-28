be7a415c7cc61dc4bb3968e271991d24b0f8d051
Kristyna Hnizdilova <kristyna.hnizdilova@plus4u.net>
2022-01-28 01:03:17 +0100

    Add Bootstrap + start with templates

--- src/main/kotlin/cz/uhk/ppro/betoffice/config/SecurityConfig.kt
+++ src/main/kotlin/cz/uhk/ppro/betoffice/config/SecurityConfig.kt
@@ -2,16 +2,15 @@ package cz.uhk.ppro.betoffice.config
 
 import cz.uhk.ppro.betoffice.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @Configuration
@@ -35,6 +34,14 @@ class SecurityConfig: WebSecurityConfigurerAdapter(){
 	@Throws(Exception::class)
 	override fun configure(auth: AuthenticationManagerBuilder) {
 		auth.authenticationProvider(authenticationProvider())
+	}
+
+	//permit resources
+	override fun configure(web: WebSecurity) {
+			web.ignoring()
+				.antMatchers(
+					"/css/**", "/fonts/**",
+					"/js/**");
 	}
 
 	override fun configure(http: HttpSecurity) {
